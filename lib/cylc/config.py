7e2b6c85c363768b6c2f97a2545bc093ef11419e
Ben Fitzpatrick <ben.fitzpatrick@metoffice.gov.uk>
2015-06-10 17:21:45 +0100

    Re-order inheritance test - hopefully acceptably

--- lib/cylc/config.py
+++ lib/cylc/config.py
@@ -788,7 +788,10 @@ class config( object ):
 
         already_done = {} # to store already computed namespaces by mro
 
-        for ns in self.cfg['runtime']:
+        # Loop through runtime members, 'root' first.
+        nses = self.cfg['runtime'].keys()
+        nses.sort(key=lambda ns: ns != 'root')
+        for ns in nses:
             # for each namespace ...
 
             hierarchy = copy(self.runtime['linearized ancestors'][ns])
